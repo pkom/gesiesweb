891dcbaa874d0f4786586a9fc99a3b8447104af0
Francisco Mora <francisco.mora.sanchez@gmail.com>
2014-11-21 14:32:51 +0100

    En las vistas de partes...

--- gesiesweb/partes/urls.py
+++ gesiesweb/partes/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 from .views import ParteListView, ParteCreateView, ParteDetailView, ParteUpdateView, ParteDeleteView,  ParteResponsableListView
 
 urlpatterns = patterns("",
-	url(regex=r'^$', view=ParteListView.as_view(), name="partes"),
-    url(regex=r'^nuevo/$', view=ParteCreateView.as_view(), name="nuevo"),
-    url(regex=r'^(?P<pk>\d+)/$', view=ParteDetailView.as_view(), name="detalle"),
-    url(regex=r'^editar/(?P<pk>\d+)/$', view=ParteUpdateView.as_view(), name="editar"),
-	url(regex=r'^responsable/$', view=ParteResponsableListView.as_view(), name="partes-responsable"),
-    url(regex=r'^responsable/eliminar/(?P<pk>\d+)/$', view=ParteDeleteView.as_view(), name="eliminar"),
+   url(regex=r'^$', view=ParteListView.as_view(), name="partes"),
+   url(regex=r'^nuevo/$', view=ParteCreateView.as_view(), name="nuevo"),
+   url(regex=r'^(?P<pk>\d+)/$', view=ParteDetailView.as_view(), name="detalle"),
+   url(regex=r'^editar/(?P<pk>\d+)/$', view=ParteUpdateView.as_view(), name="editar"),
+   url(regex=r'^responsable/$', view=ParteResponsableListView.as_view(), name="partes-responsable"),
+   url(regex=r'^responsable/eliminar/(?P<pk>\d+)/$', view=ParteDeleteView.as_view(), name="eliminar"),
 )
--- gesiesweb/partes/views.py
+++ gesiesweb/partes/views.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView
 
 from core.mixins import LoginRequerido
@@ -14,10 +15,34 @@ class ParteListView(LoginRequerido, ListView):
 
         return Parte.objects.filter(cursoprofesor=self.request.session['curso_profesor'])
 
+    def get_context_data(self, **kwargs):
+
+        context = super(ParteListView, self).get_context_data(**kwargs)
+        context['total_partes'] = Parte.objects.filter(cursoprofesor=self.request.session['curso_profesor']).count()
+        context['total_partes_partes'] = Parte.objects.filter(cursoprofesor=self.request.session['curso_profesor'],
+                                                              con_parte=True).count()
+        context['total_partes_comunicados'] = Parte.objects.filter(cursoprofesor=self.request.session['curso_profesor'],
+                                                              comunicado=True).count()
+        context['total_partes_cerrados'] = Parte.objects.filter(cursoprofesor=self.request.session['curso_profesor'],
+                                                              cerrado=True).count()
+        return context
+
 
 class ParteCreateView(LoginRequerido, CreateView):
 
-    pass
+    template_name = "partes/nuevo.html"
+    model = Parte
+    success_url = reverse_lazy('parte:partes')
+    fields = ('fecha', 'parte', 'comunicado', )
+
+    def form_valid(self, form):
+
+        return super(ParteCreateView, self).form_valid(form)
+
+
+    def form_invalid(self, form):
+
+        return super(ParteCreateView, self).form_invalid(form)
 
 
 class ParteDetailView(LoginRequerido, DetailView):
@@ -48,3 +73,14 @@ class ParteResponsableListView(LoginRequerido, ListView):
         return Parte.objects.filter(cursoprofesor__curso=self.request.session['curso_academico_usuario'])
 
 
+    def get_context_data(self, **kwargs):
+
+        context = super(ParteResponsableListView, self).get_context_data(**kwargs)
+        context['total_partes'] = Parte.objects.filter(cursoprofesor__curso=self.request.session['curso_academico_usuario']).count()
+        context['total_partes_partes'] = Parte.objects.filter(cursoprofesor__curso=self.request.session['curso_academico_usuario'],
+                                                              con_parte=True).count()
+        context['total_partes_comunicados'] = Parte.objects.filter(cursoprofesor__curso=self.request.session['curso_academico_usuario'],
+                                                              comunicado=True).count()
+        context['total_partes_cerrados'] = Parte.objects.filter(cursoprofesor__curso=self.request.session['curso_academico_usuario'],
+                                                              cerrado=True).count()
+        return context
